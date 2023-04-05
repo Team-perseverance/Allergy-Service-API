9f638a083b4bdd60860c277669ab8d79041411a9
bhanuprakashkola26 <bhanuprakashkola26@gmail.com>
2023-04-05 11:30:32 +0530

    Allergy Services

---
+++ AllergyApiTest/AllergyMapperTest.cs
@@ -0,0 +1,47 @@
+﻿using AutoFixture;
+using Moq;
+using FluentAssertions;
+using AllergyServiceApi.Controllers;
+using BusinessLogic;
+using Models;
+using df = DataFluentApi;
+using Xunit;
+using DataFluentApi.Entities;
+
+namespace AllergyApiTest
+{
+    public class AllergyMapperTest
+    {
+        [Fact]
+        public void EntitiesToModels()
+        {
+            df.Entities.Allergy map = new df.Entities.Allergy();
+            var result = Mapper.Map(map);
+            Assert.Equal(result.GetType(),typeof(Models.Allergy));
+        }
+
+        [Fact]
+        public void ModelsToEntities()
+        {
+            Models.Allergy map = new Models.Allergy();
+            var result = Mapper.Map(map);
+            Assert.Equal(result.GetType(),typeof(df.Entities.Allergy));
+        }
+
+        [Fact]
+        public void Map_ReturnsCorrectType()
+        {
+            // Arrange
+            var allergyEntities = new List<DataFluentApi.Entities.Allergy>
+            {
+                new DataFluentApi.Entities.Allergy { AllergyId = Guid.NewGuid(), AllergyName = "Peanuts" },
+            };
+
+            // Act
+            var result = Mapper.Map(allergyEntities);
+
+            // Assert
+            Assert.IsType<List<Models.Allergy>>(result.ToList());
+        }
+    }
+}
